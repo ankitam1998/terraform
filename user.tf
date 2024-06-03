b4148ac6975403eaffeae075f684c4082fd02357
ankitam1998 <166798179+ankitam1998@users.noreply.github.com>
2024-06-03 14:16:05 +0530

    Update user.tf

--- user.tf
+++ user.tf
@@ -1,32 +1,39 @@
 provider "aws" {
   region = "us-east-1"
 }
-resource "aws_iam_user" "ak" {
-  name = "ankita"
+resource "aws_iam_user" "hm" {
+  name = "hema"
   path = "/system/"
-
   tags = {
     tag-key = "tag-value"
   }
 }
-
-resource "aws_iam_access_key" "ak" {
-  user = aws_iam_user.ak.name
+resource "aws_iam_access_key" "hm" {
+  user = aws_iam_user.hm.name
+}
+data "aws_iam_policy_document" "hm_ro" {
+  statement  {
+        effect = "Allow"
+        actions = ["*"]
+        resources = ["*"]
+        }
+}
+resource "aws_iam_user_policy" "hm_ro" {
+  name   = "administratorfullaccess"
+  user   = aws_iam_user.hm.name
+  policy = data.aws_iam_policy_document.hm_ro.json
+}
+resource "aws_key_pair" "deployer" {
+  key_name   = "deployer-key"
+  public_key = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQD3F6tyPEFEzV0LX3X8BsXdMsQz1x2cEikKDEY0aIj41qgxMCP/iteneqXSIFZBp5vizPvaoIR3Um9xK7PGoW8giupGn+EPuxIA4cDM4vzOqOkiMPhz5XK0whEjkVzTo4+S0puvDZuwIsdiW9mxhJc7tgBNL0cYlWSYVkz4G/fslNfRPW5mYAM49f4fhtxPb5ok4Q2Lg9dPKVHO/Bgeu5woMc7RY0p1ej6D4CKFE6lymSDJpW0YHX/wqE9+cfEauh7xZcG0q9t2ta6F6fmX0agvpFyZo8aFbXeUBr7osSCJNgvavWbM/06niWrOvYX2xwWdhXmXSrbX8ZbabVohBK41 email@example.com"
 }
 
-data "aws_iam_policy_document" "ak_ro" {
-  statement {
-    effect    = "Allow"
-    actions   = ["*"]
-    resources = ["*"]
-  }
-}
 
-resource "aws_iam_user_policy" "ak_ro" {
-  name   = "adfullaccess"
-  user   = aws_iam_user.ak.name
-  policy = data.aws_iam_policy_document.ak_ro.json
-}
+
+       
+     
+
+ 
 
 
 
